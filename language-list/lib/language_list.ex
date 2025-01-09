9dccb2f026882526f6e4a0aaaa1bdcd3b4c7ef39
Chris <chris.t.evans@icloud.com>
2025-01-08 22:19:38 -0700

    Implement core functions for Language List exercise with tests and project structure

--- language-list/lib/language_list.ex
+++ language-list/lib/language_list.ex
@@ -1,25 +1,8 @@
 defmodule LanguageList do
-  def new() do
-    # Please implement the new/0 function
-  end
-
-  def add(list, language) do
-    # Please implement the add/2 function
-  end
-
-  def remove(list) do
-    # Please implement the remove/1 function
-  end
-
-  def first(list) do
-    # Please implement the first/1 function
-  end
-
-  def count(list) do
-    # Please implement the count/1 function
-  end
-
-  def functional_list?(list) do
-    # Please implement the functional_list?/1 function
-  end
+  def new(), do: []
+  def add(list, language), do: [language | list]
+  def remove([_hd | tl]), do: tl
+  def first([hd | _tl]), do: hd
+  def count(list), do: length(list)
+  def functional_list?(list), do: "Elixir" in list
 end
