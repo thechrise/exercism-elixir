8c53bf47d19646b8016a8f6820209e71492ecd8b
Chris <chris.t.evans@icloud.com>
2025-01-10 16:21:46 -0700

    HighScore module solution

--- high-score/lib/high_score.ex
+++ high-score/lib/high_score.ex
@@ -1,25 +1,13 @@
 defmodule HighScore do
-  def new() do
-    # Please implement the new/0 function
-  end
+  @starting_score 0
 
-  def add_player(scores, name, score) do
-    # Please implement the add_player/3 function
-  end
+  def new(), do: %{}
+  def add_player(scores, name, score \\ @starting_score), do: Map.put(scores, name, score)
+  def remove_player(scores, name), do: Map.delete(scores, name)
+  def reset_score(scores, name), do: Map.put(scores, name, @starting_score)
 
-  def remove_player(scores, name) do
-    # Please implement the remove_player/2 function
-  end
+  def update_score(scores, name, score),
+    do: Map.update(scores, name, score, fn current_score -> current_score + score end)
 
-  def reset_score(scores, name) do
-    # Please implement the reset_score/2 function
-  end
-
-  def update_score(scores, name, score) do
-    # Please implement the update_score/3 function
-  end
-
-  def get_players(scores) do
-    # Please implement the get_players/1 function
-  end
+  def get_players(scores), do: Enum.map(scores, fn {name, _score} -> name end)
 end
