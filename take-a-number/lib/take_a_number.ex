a9a2ccc085d7865cc57313278e9f2f9e9d6f2335
Chris <chris.t.evans@icloud.com>
2025-01-11 22:29:38 -0700

    Take A Number solution

--- take-a-number/lib/take_a_number.ex
+++ take-a-number/lib/take_a_number.ex
@@ -1,5 +1,24 @@
 defmodule TakeANumber do
   def start() do
-    # Please implement the start/0 function
+    spawn(fn -> loop(0) end)
+  end
+
+  defp loop(state) do
+    receive do
+      {:report_state, sender} ->
+        send(sender, state)
+        loop(state)
+
+      {:take_a_number, sender} ->
+        new_state = state + 1
+        send(sender, new_state)
+        loop(new_state)
+
+      :stop ->
+        nil
+
+      _ ->
+        loop(state)
+    end
   end
 end
