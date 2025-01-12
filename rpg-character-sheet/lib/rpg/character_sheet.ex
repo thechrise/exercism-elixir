f2fa9a2234fae31582e687e183bc721a6d4d1f97
Chris <chris.t.evans@icloud.com>
2025-01-11 21:58:41 -0700

    Complete RPG Character

--- rpg-character-sheet/lib/rpg/character_sheet.ex
+++ rpg-character-sheet/lib/rpg/character_sheet.ex
@@ -1,21 +1,29 @@
 defmodule RPG.CharacterSheet do
   def welcome() do
-    # Please implement the welcome/0 function
+    IO.puts("Welcome! Let's fill out your character sheet together.")
   end
 
   def ask_name() do
-    # Please implement the ask_name/0 function
+    name = IO.gets("What is your character's name?\n")
+    name |> String.trim()
   end
 
   def ask_class() do
-    # Please implement the ask_class/0 function
+    class = IO.gets("What is your character's class?\n")
+    class |> String.trim()
   end
 
   def ask_level() do
-    # Please implement the ask_level/0 function
+    level = IO.gets("What is your character's level?\n")
+    level |> String.trim() |> String.to_integer()
   end
 
   def run() do
-    # Please implement the run/0 function
+    welcome()
+    name = ask_name()
+    class = ask_class()
+    level = ask_level()
+    character_map = %{class: class, level: level, name: name}
+    IO.inspect(character_map, label: "Your character")
   end
 end
