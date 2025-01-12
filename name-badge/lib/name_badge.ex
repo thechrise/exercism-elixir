b6487e292df6b3e91090d7c77ea6156e09f5276c
Chris <chris.t.evans@icloud.com>
2025-01-11 22:12:59 -0700

    Name Badge solution

--- name-badge/lib/name_badge.ex
+++ name-badge/lib/name_badge.ex
@@ -1,5 +1,16 @@
 defmodule NameBadge do
   def print(id, name, department) do
-    # Please implement the print/3 function
+    id_string = get_id_string(id)
+    department_string = get_department_string(department)
+
+    "#{id_string} #{name} - #{department_string}" |> String.trim()
+  end
+
+  defp get_id_string(id) do
+    if id != nil, do: "[#{id}] -", else: ""
+  end
+
+  defp get_department_string(department) do
+    if department != nil, do: String.upcase(department), else: "OWNER"
   end
 end
