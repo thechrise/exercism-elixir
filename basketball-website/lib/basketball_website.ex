b4010f897f80d8f80b4f8f64584e33c498484630
Chris <chris.t.evans@icloud.com>
2025-01-25 10:42:02 -0700

    Basketball Website solution

--- basketball-website/lib/basketball_website.ex
+++ basketball-website/lib/basketball_website.ex
@@ -1,9 +1,13 @@
 defmodule BasketballWebsite do
   def extract_from_path(data, path) do
-    # Please implement the extract_from_path/2 function
+    extract_from_list(data, String.split(path, "."))
   end
 
+  defp extract_from_list(nil, _), do: nil
+  defp extract_from_list(data, []), do: data
+  defp extract_from_list(data, [key | tail]), do: extract_from_list(data[key], tail)
+
   def get_in_path(data, path) do
-    # Please implement the get_in_path/2 function
+    Kernel.get_in(data, String.split(path, "."))
   end
 end
