c1507fc70bf51b826f647f7fc48afaf4dfc1bcfe
Chris <chris.t.evans@icloud.com>
2025-01-26 14:43:37 -0700

    Library Fees solution

--- library-fees/lib/library_fees.ex
+++ library-fees/lib/library_fees.ex
@@ -1,25 +1,46 @@
 defmodule LibraryFees do
+  @checkout_days 28
+
   def datetime_from_string(string) do
-    # Please implement the datetime_from_string/1 function
+    NaiveDateTime.from_iso8601!(string)
   end
 
   def before_noon?(datetime) do
-    # Please implement the before_noon?/1 function
+    datetime.hour < 12
   end
 
   def return_date(checkout_datetime) do
-    # Please implement the return_date/1 function
+    checkout_datetime
+    |> NaiveDateTime.to_date()
+    |> Date.add(checkout_duration(checkout_datetime))
   end
 
   def days_late(planned_return_date, actual_return_datetime) do
-    # Please implement the days_late/2 function
+    actual_return_datetime
+    |> Date.diff(planned_return_date)
+    |> max(0)
   end
 
   def monday?(datetime) do
-    # Please implement the monday?/1 function
+    datetime
+    |> NaiveDateTime.to_date()
+    |> Date.day_of_week() == 1
   end
 
   def calculate_late_fee(checkout, return, rate) do
-    # Please implement the calculate_late_fee/3 function
+    expected_return = datetime_from_string(checkout) |> return_date()
+    actual_return = datetime_from_string(return)
+
+    final_rate = if monday?(actual_return), do: rate * 0.5, else: rate
+
+    (days_late(expected_return, actual_return) * final_rate)
+    |> floor()
+  end
+
+  defp checkout_duration(datetime) do
+    case before_noon?(datetime) do
+      true -> @checkout_days
+      false -> @checkout_days + 1
+    end
   end
 end
